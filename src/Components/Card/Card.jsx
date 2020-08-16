d29d2d489325fba28f8c28cc21c724eb0524d0ff
Bill <ceriustech@gmail.com>
2020-08-16 18:07:59 -0500

    Added styling to the navigation and cardlist component.

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
+import ArrowLeftIcon from "@material-ui/icons/ArrowLeft";
 import "./Card.Styles.css";
 
 const Card = (props) => (
   <div className="card-container">
     <h2>{props.category}</h2>
     <p>Some info.</p>
-    <p>Look at {props.category} api's</p>
+    <p>
+      <span className="card-list-span_left">Click Here</span>{" "}
+      <span className="card-list-span_right">
+        <ArrowLeftIcon />
+      </span>
+    </p>
   </div>
 );
 
