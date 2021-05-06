0bf85ff60ce50c41224c81808c7ccb467a943c44
Bill <ceriustech@gmail.com>
2021-05-05 23:13:34 -0500

    Changed row and column height and width on subcategory page.

---
+++ src/Components/Buttons/URL-Buttons.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './URL-Buttons.styles.css';
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+
+const URLButtons = () => {
+	return (
+		<div className="button-link-container">
+			<a
+				className="button-link"
+				href="https://developers.amadeus.com/self-service"
+				target="_blank"
+			>
+				<p className="button-link_p">
+					<span className="button-link-span_left">Click Here</span>
+					<span className="button-link-span_right">
+						<ArrowLeftIcon />
+					</span>
+				</p>
+			</a>
+		</div>
+	);
+};
+
+export default URLButtons;
--- src/Components/Sub-Category/Sub-Category.jsx
+++ src/Components/Sub-Category/Sub-Category.jsx
@@ -1,25 +1,26 @@
-import React from "react";
-
-import { Link } from "react-router-dom";
+import React from 'react';
 
 
-import "./Sub-Category.Styles.css";
+import URLButtons from '../Buttons/URL-Buttons';
+
+import './Sub-Category.Styles.css';
 
 const SubCategory = (props) => {
+	return (
+		<section className="sub-category-container">
+			<div className="sub-category-info">
+				<p className="sub-category-info-text">API Name</p>
+				<p className="sub-category-info-text">
+					Access real-time and historical data of any and all airborne aircraft
+				</p>
+				<p className="sub-category-info-text">Yes</p>
+				<p className="sub-category-info-text">Yes</p>
+				<p className="sub-category-info-text">Yes</p>
+				
+					<URLButtons />
+			</div>
+		</section>
+	);
+};
 
-
-  return (
-      <section className="sub-category-container">
-          <div className="sub-category-info">
-              <p className="sub-category-info-text">API Name</p>
-              <p className="sub-category-info-text">Some Description</p>
-              <p className="sub-category-info-text">Yes</p>
-              <p className="sub-category-info-text">Yes</p>
-              <p className="sub-category-info-text">Yes</p>
-              <p className="sub-category-info-text">www.example.com</p>
-          </div>
-      </section>
-  )
-}
-
-export default SubCategory; 
+export default SubCategory;
